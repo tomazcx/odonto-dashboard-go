4dff90a8bf872d7577d61f2b3dcc21da5338c467
tomazcx <tomazcx06@gmail.com>
2023-11-15 12:33:16 -0300

    feat: added base and dashboard layout

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/tomazcx/odonto-dashboard-go/views"
+	"github.com/tomazcx/odonto-dashboard-go/views/layouts"
 )
 
 func main() {
 
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 	http.Handle("/login", templ.Handler(views.Login()))
+	http.Handle("/", templ.Handler(layouts.DashboardLayout()))
 
 	fmt.Println("Server running at port 8000!")
 	http.ListenAndServe(":8000", nil)
