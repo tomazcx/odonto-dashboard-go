049b527e053be9e1883e6452ad62c8874c19b0e0
tomazcx <tomazcx06@gmail.com>
2023-11-14 21:52:41 -0300

    feat: added support to go templ, improved rot heload

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomazcx/odonto-dashboard-go
 
 go 1.21.2
+
+require github.com/a-h/templ v0.2.432
--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/tomazcx/odonto-dashboard-go/views"
 )
 
 func main() {
 
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template.Must(template.ParseFiles("./index.html")).Execute(w, nil)
-	})
+	http.Handle("/login", templ.Handler(views.Login()))
 
+	fmt.Println("Server running at port 8000!")
 	http.ListenAndServe(":8000", nil)
 }
